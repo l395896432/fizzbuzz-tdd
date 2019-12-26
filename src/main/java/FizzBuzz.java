f71cbe796845c5c0fa2d851799aa187ac09a1921
Seaborn Lee <seabornlee.cn@gmail.com>
2019-12-26 17:21:37 +0800

    test: parameterized tests

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static String of(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
+    }
+}
